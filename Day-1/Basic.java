8c80cfaab021bb8c96a5cfe2ec0a2a886bdb8b96
Ankita Das <daszankita23@gmail.com>
2024-11-13 20:35:29 +0530

    Basics in java

---
+++ Day-1/Basic.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+public class Basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //String input=sc.nextLine();
+        //next only capture till space when you want to store space you use nextLine()
+        //int number=sc.nextInt();
+        //for integers its nextInt()
+        //for float its nextFloat()
+        //System.out.println(number);
+        //Byte var = sc.nextByte();
+       // System.out.println(var);
+        //byte used to store small integers type data
+    //Boolean value=sc.nextBoolean();
+   // System.out.println(value);
+    //in boolean you have to enter true or false
+    //Long val=sc.nextLong();
+    //System.out.println(val);
+    }
+    
+}
---
+++ Day-1/Javabasic.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Javabasic{
+    public static void main(String[]args){
+    int a=10;
+    int b=5;
+    int sum=a+b;
+    System.out.println("sum is:"+sum);
+    }
+}
