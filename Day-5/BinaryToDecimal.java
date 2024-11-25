9c97487732cd19718b51d53b64afd684e4a980b2
Ankita Das <daszankita23@gmail.com>
2024-11-25 22:25:45 +0530

    binary to decimal

--- Day-5/BinaryToDecimal.java
+++ Day-5/BinaryToDecimal.java
@@ -1,7 +1,16 @@
 import java.util.*;
 public class BinaryToDecimal {
-    
-    public static void main(String[] args) {
-        
+    public static int BTD(int n){
+        int num,pow=0,dec=0;
+        while(n!=0){
+        num=n%10;
+        dec=dec+(num*(int)(Math.pow(2, pow)));
+        pow++;
+        n=n/10;
+        }
+       return dec;
+    } 
+        public static void main(String[] args) {
+        System.out.println(BTD(11));
     }
 }
