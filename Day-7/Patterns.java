244fea997eb41052252b19aaf8d59a79e7850370
Ankita Das <daszankita23@gmail.com>
2024-11-27 22:03:06 +0530

    HOLLOW RECTANGLE

---
+++ Day-7/Patterns.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Patterns {
+    public static void Hollowrectangle(int r, int c){
+        int i,j;
+        for(i=1;i<=r;i++){
+            for(j=1;j<=c;j++){
+                if(i==1||i==r||j==1||j==c){
+                    System.out.print("*"+" ");
+                }
+                else{
+                    System.out.print(" "+" ");
+                }10
+                
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+    Hollowrectangle(4, 04);
+    }
+}
