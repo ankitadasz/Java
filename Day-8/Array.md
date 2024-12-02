552dab589623c8ab2b3c407f5c33aaddcca384a5
Ankita Das <daszankita23@gmail.com>
2024-12-02 20:51:26 +0530

    Linear search

---
+++ Day-8/Array.md
@@ -0,0 +1,2 @@
+Array is the list of element of same type placed in a contegeous memory allocation
+creating an array: datatype arrayName[]=new datattype[size]
---
+++ Day-9/LinearSearch.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LinearSearch {
+
+    public static void LinearSearch(int arr[], int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("the key" + key + "is present at the place" + (i+1));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+     int arr[]={2,3,4,5,6};
+     int key=6;
+     LinearSearch(arr, key);
+    }
+}
