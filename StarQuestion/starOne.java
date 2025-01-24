fcd029f5e8ca19040077989beea3e2090ee0ed75
Ankita Das <daszankita23@gmail.com>
2025-01-24 20:00:48 +0530

    Star Question

---
+++ StarQuestion/starOne.java
@@ -0,0 +1,12 @@
+public class StarOne {
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for(i=0;i<n;i++){
+            for(j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            
+        }
+    }
+}
