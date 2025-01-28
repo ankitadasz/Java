4ff1b1af375e11e1f841bd390edf751fd5225e8d
Ankita Das <daszankita23@gmail.com>
2025-01-28 20:27:32 +0530

    number pyramid pattern

---
+++ Day-8/patternFour.java
@@ -0,0 +1,12 @@
+public class patternFour {
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for(i=1;i<=n;i++){
+            for(j=i;j>=1;j--){
+                System.out.print(" "+j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
